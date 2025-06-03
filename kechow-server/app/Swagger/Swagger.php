a619daf7e0901ab757e393ee1c0756b075cea90f
Abimael <abimael1992g@gmail.com>
2025-06-02 19:15:07 -0600

    Add Swagger setup

---
+++ kechow-server/app/Swagger/Swagger.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Swagger;
+
+use OpenApi\Annotations as OA;
+
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="Kechow API",
+ *     description="API documentation for Kechow",
+ *     @OA\Contact(
+ *         email="support@kechow.com"
+ *     )
+ * )
+ */
+class Swagger {}
