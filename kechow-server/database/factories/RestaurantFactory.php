d498a214b9e1791bc791723a1e9c59883bdf683b
Abimael <abimael1992g@gmail.com>
2025-06-18 19:12:35 -0600

    🧪 Add Restaurant factory for testing and seeding

---
+++ kechow-server/database/factories/RestaurantFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Restaurant;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RestaurantFactory extends Factory
+{
+    protected $model = Restaurant::class;
+
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->company(),
+            'description' => $this->faker->sentence(),
+            'address' => $this->faker->address(),
+            'city' => $this->faker->city(),
+            'state' => $this->faker->state(),
+            'zip_code' => $this->faker->postcode(),
+            'phone' => $this->faker->phoneNumber(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'website' => $this->faker->url(),
+            'opening_time' => '09:00:00',
+            'closing_time' => '22:00:00',
+            'logo_url' => null,
+            'is_active' => true,
+        ];
+    }
+}
