d4b88f2537715ee6215b93b640d66dd011e5be70
Abimael <abimael1992g@gmail.com>
2025-06-23 11:57:59 -0600

    fixed home page

--- kechow-client/src/pages/HomePage.vue
+++ kechow-client/src/pages/HomePage.vue
@@ -4,7 +4,7 @@
       <!-- Header -->
       <header class="mb-10 flex justify-between items-center">
         <h1 class="text-3xl font-bold bg-gradient-to-r from-indigo-500 to-purple-500 bg-clip-text text-transparent">
-          Epicure
+          Restaurants
         </h1>
         <button class="p-2 rounded-full hover:bg-gray-200 dark:hover:bg-gray-800 transition">
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round">
