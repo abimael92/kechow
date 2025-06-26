620661a2d1244e92892c017d00e82d5c4e554a97
Abimael <abimael1992g@gmail.com>
2025-06-26 09:34:34 -0600

    🔧 Set up App.vue to use <router-view /> only

--- kechow-client/src/App.vue
+++ kechow-client/src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="min-h-screen bg-bg-light text-text-light dark:bg-bg-dark dark:text-text-dark font-sans">
-    <router-view />
-  </div>
+	<div
+		class="min-h-screen bg-bg-light text-text-light dark:bg-bg-dark dark:text-text-dark font-sans"
+	>
+		<router-view />
+	</div>
 </template>
-
-<script setup>
-// No script needed
-</script>
