0601eed0973f724791b2111adbc5de50dcf3ca45
Abimael <abimael1992g@gmail.com>
2025-07-08 19:00:15 -0600

    🗂️ chore: add SVG index export file

---
+++ kechow-client/src/assets/svg/food/index.ts
@@ -0,0 +1,7 @@
+// src/assets/svg/food/index.js
+import Seafood from './seafood.svg';
+import MexicanFood from './mexicanfood.svg';
+import FastFood from './fastfood.svg';
+import Vegan from './veganfood.svg';
+
+export const categoryIcons = { Seafood, MexicanFood, FastFood, Vegan };
