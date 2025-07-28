41cf411d97272fb20b9b839b212fc90e4477f4f7
Abimael <abimael1992g@gmail.com>
2025-07-28 12:17:08 -0600

    �� Add HasApiTokens to User model to support token creation

--- kechow-server/app/Models/User.php
+++ kechow-server/app/Models/User.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;  // <-- add this
 use OpenApi\Annotations as OA;
 
 /**
@@ -23,8 +24,7 @@ use OpenApi\Annotations as OA;
  */
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable;  // <-- add HasApiTokens here
 
     protected $fillable = [
         'name',
