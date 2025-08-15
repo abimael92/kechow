075ea4fbdc2e37c3ff65839f1a1c7525fbd4562c
Abimael Garcia <abimael1992g@gmail.com>
2025-08-14 23:30:06 -0600

    🖥️ chore: update AppServiceProvider config

--- kechow-server/app/Providers/AppServiceProvider.php
+++ kechow-server/app/Providers/AppServiceProvider.php
@@ -17,8 +17,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        if (env('APP_DEBUG')) {
+            \Route::get('/debug/routes', function() {
+                return \Route::getRoutes()->getRoutesByName();
+            });
+        }
     }
 }
