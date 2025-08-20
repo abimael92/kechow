4abf224eeb33798dc08b08b6a1bff9c7d8347058
Abimael Garcia <abimael1992g@gmail.com>
2025-08-19 18:59:38 -0600

    ⚙️ Update main.ts to use new AppConfig

--- kechow-client/src/main.ts
+++ kechow-client/src/main.ts
@@ -1,23 +1,16 @@
 import { createApp } from 'vue';
 import App from './App.vue';
-import router from './router';
-import { createI18n } from 'vue-i18n';
+import router from './app/router';
 import { createPinia } from 'pinia';
-import './styles/global.css';
+import './assets/styles/global.css';
 
-import en from './locales/en.json';
-import es from './locales/es.json';
+import { AppConfig, ThemeConfig, i18n } from './app/config';
 
-const i18n = createI18n({
-	legacy: false,
-	locale: 'en-US',
-	fallbackLocale: 'en-US',
-	messages: {
-		'en-US': en,
-		'es-ES': es,
-	},
-});
+const app = createApp(App);
 
-const pinia = createPinia();
+app.use(createPinia());
+app.use(router);
+app.use(i18n);
+ThemeConfig.applyTheme();
 
-createApp(App).use(router).use(i18n).use(pinia).mount('#app');
+app.mount('#app');
