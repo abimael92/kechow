a0990aa16b48f3d0acf1e25981fecf42de638467
Abimael Garcia <abimael1992g@gmail.com>
2025-08-19 18:58:09 -0600

    🧩 Add IconClose component to shared UI

---
+++ kechow-client/src/shared/ui/IconClose.vue
@@ -0,0 +1,15 @@
+<template>
+	<svg
+		class="h-6 w-6"
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		stroke-width="2"
+	>
+		<path
+			stroke-linecap="round"
+			stroke-linejoin="round"
+			d="M6 18L18 6M6 6l12 12"
+		/>
+	</svg>
+</template>
