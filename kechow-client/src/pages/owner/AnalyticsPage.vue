634f046696150cd0fdd26c19aec4e747b1357894
Abimael Garcia <abimael1992g@gmail.com>
2025-09-04 12:30:49 -0600

    📄 Add AnalyticsPage integrating analytics components

---
+++ kechow-client/src/pages/owner/AnalyticsPage.vue
@@ -0,0 +1,9 @@
+<template>
+	<div class="p-6">
+		<AnalyticsPanel />
+	</div>
+</template>
+
+<script setup lang="ts">
+import AnalyticsPanel from '@/features/business-owner/containers/AnalyticsPanel.vue';
+</script>
