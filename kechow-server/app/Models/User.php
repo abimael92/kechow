ec89f0bb70e1a389d87d038219a9126ab3d0fa27
Abimael Garcia <abimael1992g@gmail.com>
2025-09-10 16:01:18 -0600

    📝 Update User.php model for Sanctum tokens

--- kechow-server/app/Models/User.php
+++ kechow-server/app/Models/User.php
@@ -53,11 +53,8 @@ class User extends Authenticatable
      *
      * @var array<string, string>
      */
-    protected function casts(): array
-    {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
-    }
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
 }
