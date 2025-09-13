fd54f9625ceb060ffbc9fee1228008a59508ba81
Abimael Garcia <abimael1992g@gmail.com>
2025-09-12 20:23:08 -0600

    🗑️ Removed deprecated settings page component

--- kechow-client/src/pages/owner/SettingsPage.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div class="p-6">
-		<SettingsPanel />
-	</div>
-</template>
-
-<script setup lang="ts">
-import SettingsPanel from '@/features/business-owner/containers/SettingsPanel.vue';
-</script>
