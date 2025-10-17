28224a2e2af75429d249f804a649001a639c2bdd
Abimael Garcia <abimael1992g@gmail.com>
2025-10-16 20:26:23 -0600

    ⚙️ Configure toastification in main.ts

--- kechow-client/src/main.ts
+++ kechow-client/src/main.ts
@@ -6,11 +6,26 @@ import './assets/styles/global.css';
 
 import { AppConfig, ThemeConfig, i18n } from './app/config';
 
+import Toast from 'vue-toastification';
+import 'vue-toastification/dist/index.css';
+
 const app = createApp(App);
+
+const toastOptions = {
+	position: 'top-center',
+	timeout: 3000,
+	closeOnClick: true,
+	pauseOnHover: true,
+	draggable: true,
+	hideProgressBar: false,
+	transition: 'Vue-Toastification__fade',
+};
 
 app.use(createPinia());
 app.use(router);
 app.use(i18n);
+app.use(Toast as any, toastOptions);
+
 ThemeConfig.applyTheme();
 
 app.mount('#app');
