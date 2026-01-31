023a815fc60d9911a07435b325e18c6a9fdb8c14
Abimael Garcia <abimael1992g@gmail.com>
2026-01-31 13:32:05 -0600

    owner: add controllers

---
+++ kechow-server/app/Modules/Owner/Controllers/OwnerController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Modules\Owner\Controllers;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * Owner feature controller (CRUD placeholder).
+ * Owner-specific actions (e.g. dashboard, my restaurants, settings) will be implemented here.
+ * Does not replace Restaurant CRUD; complements it with owner-scoped endpoints.
+ */
+class OwnerController extends Controller
+{
+    //
+}
