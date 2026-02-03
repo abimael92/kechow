a916fdea202650e7e29d3dd5d706b3cd02583d64
Abimael Garcia <abimael1992g@gmail.com>
2026-02-03 09:34:28 -0600

    refactor align backend authentication logic

--- kechow-server/app/Models/User.php
+++ kechow-server/app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -25,7 +24,7 @@ use OpenApi\Annotations as OA;
  */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
