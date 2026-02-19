143435e55bc270d3657e243b69d517590656471d
Abimael Garcia <abimael1992g@gmail.com>
2026-02-18 19:03:25 -0600

    chore update order and user seeders for delivery flow

--- kechow-server/database/seeders/UserSeeder.php
+++ kechow-server/database/seeders/UserSeeder.php
@@ -4,14 +4,40 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    public function run(): void
+    public function run()
     {
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // CLIENTE
+        User::updateOrCreate(
+            ['email' => 'cliente@cliente.com'],
+            [
+                'name'     => 'Abimael Cliente',
+                'password' => Hash::make('123456'),
+                'role'     => 'customer'
+            ]
+        );
+
+        // REPARTIDOR (DRIVER)
+        User::updateOrCreate(
+            ['email' => 'entrega@entrega.com'],
+            [
+                'name'     => 'deliverboy',
+                'password' => Hash::make('123456'),
+                'role'     => 'driver'
+            ]
+        );
+
+        // NEGOCIO (OWNER)
+        User::updateOrCreate(
+            ['email' => 'negocio@negocio.com'],
+            [
+                'name'     => 'don negocio',
+                'password' => Hash::make('123456'),
+                'role'     => 'owner'
+            ]
+        );
     }
 }
