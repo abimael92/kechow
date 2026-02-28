b5b72732fa91a6098de6aede9381b1f1b5f66c55
Abimael Garcia <abimael1992g@gmail.com>
2026-02-27 19:55:21 -0600

    chore update sanctum configuration for token handling

--- kechow-server/config/sanctum.php
+++ kechow-server/config/sanctum.php
@@ -15,12 +15,12 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
-        '%s%s',
-        '127.0.0.1,127.0.0.1:3000,127.0.0.1,127.0.0.1:8000,::1',
-        Sanctum::currentApplicationUrlWithPort(),
-        // Sanctum::currentRequestHost(),
-    ))),
+    'stateful' => [
+        'localhost:5173',
+        '127.0.0.1:5173',
+        'localhost:8000',
+        '127.0.0.1:8000',
+    ],
 
     /*
     |--------------------------------------------------------------------------
